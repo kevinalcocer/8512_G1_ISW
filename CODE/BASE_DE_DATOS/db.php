7ee6996e6ae93ad84602e0dc1295e032c2503d7b
Kevin Alcocer <104943152+kevinalcocer@users.noreply.github.com>
2023-03-01 16:53:35 -0500

    Add files via upload
    Se sube codigo de la base de datos

--- CODE/BASE_DE_DATOS/db.php
+++ CODE/BASE_DE_DATOS/db.php
@@ -1,16 +1,3 @@
 <?php 
-//Configuracion para acceder a la bd
-function conn(){
-$hostname = "localhost";
-$usuariodb = "root";
-$passworddb ="";
-$dbname = "registro";
-
-    //Conectar con el servidor
-
-    $conectar =mysqli_connect($hostname, $usuariodb, $passworddb, $dbname);
-    return $conectar;
-}
-
-
+$conexion=mysqli_connect( "localhost", "root", "", "formulario1")
 ?>
